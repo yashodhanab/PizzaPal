7f694c19075e5ffa16bf289cbc329f483b1a3c3c
yashodhanab <p.yashodhana@gmail.com>
2024-12-31 02:36:40 +0530

    test2

--- pizzapal/src/App.jsx
+++ pizzapal/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App() {/*
   const [count, setCount] = useState(0)
 
   return (
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  )*/
 }
 
 export default App
